82eae9b51b8576f786ce946f6a12deea3415af37
taraskunuts <117195745+taraskunuts@users.noreply.github.com>
2025-02-15 12:42:15 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -14,35 +14,4 @@ backdrop.addEventListener('click', (event) => {
         backdrop.classList.remove('is-visible')
     }
 })
-//3
-const colorRadios = document.querySelectorAll('input[name="color"]')
-colorRadios.forEach(radio => {
-    radio.addEventListener('change', () => {
-        const selectedColor = document.querySelector('input[name="color"]:checked').value
-        document.body.style.backgroundColor = selectedColor
-    })
-})
-//4
-const nameInput = document.getElementById('name-input')
-const nameOutput = document.getElementById('name-output')
-nameInput.addEventListener('input', () => {
-    nameOutput.textContent = nameInput.value.trim() === '' ? '???' : nameInput.value
-})
-const validationInput = document.getElementById('validation-input')
-validationInput.addEventListener('blur', () => {
-    const length = validationInput.getAttribute('data-length')
-    const inputLength = validationInput.value.length
-    if (inputLength === Number(length)) {
-        validationInput.classList.add('valid')
-        validationInput.classList.remove('invalid')
-    } else {
-        validationInput.classList.add('invalid')
-        validationInput.classList.remove('valid')
-    }
-})
-//5
-const fontSizeControl = document.getElementById('font-size-control');
-const textSpan = document.getElementById('text')
-fontSizeControl.addEventListener('input', () => {
-    textSpan.style.fontSize = `${fontSizeControl.value}px`
-})
+
